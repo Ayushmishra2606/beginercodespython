cbc94e20c6ee8db876d302cf8907b6e6c52c0397
Ayushmishra2606 <142798638+Ayushmishra2606@users.noreply.github.com>
2023-09-03 20:21:06 +0530

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,7 +1,7 @@
 a=float(input("enter the value of base of triangle"))
 b=float(input("enter the value of height of triangle"))
 
-h=(a**2+b**2)**1/2
+h=(a**2+b**2)**0.5
 
 print("hypotenuse of triangle",h)
 input()
